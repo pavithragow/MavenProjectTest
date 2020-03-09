c844c198fb3331d0904405a1464ee1f08c2c6d49
deepak <seleniumoar1234@gmail.com>
2020-03-09 18:13:41 +0530

    push

--- mvnProject/src/test/java/com/qspiders/pactest/Sample1Test.java
+++ mvnProject/src/test/java/com/qspiders/pactest/Sample1Test.java
@@ -7,4 +7,16 @@ public class Sample1Test {
 	public void sample3test() {
 	System.out.println("test1");
 	}
+	
+	@Test
+	public void sample5test() {
+	System.out.println("test5");
+	}
+
+
+@Test
+public void sample6test() {
+System.out.println("test6");
+}
+
 }
